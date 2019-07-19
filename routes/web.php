d109079d2c5e22d5f4f090d4bf0de759afeb1849
Benjamin Stout <ben.stout@alliedhealthmedia.com>
2019-07-18 17:46:25 -0700

    Build out Oauth2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/settings', 'SettingsController@index')->name('settings');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
